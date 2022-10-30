63620178094835e782ff505f2b6f665b2c7b710e
Richard Westmoreland <richard.westmoreland@scottscheapflights.com>
2022-10-30 14:55:04 -0500

    changes

---
+++ frontend/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    newNextLinkBehavior: true,
+    scrollRestoration: true,
+  },
+  images: {
+    domains: ['admin.richardawestmoreland.com'],
+  },
+};
+
+module.exports = nextConfig;
