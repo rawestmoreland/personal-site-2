8ca1c626d23c24c6dc50bc58976df497078f6174
Richard Westmoreland <rawestmoreland@gmail.com>
2024-09-09 09:53:39 +0100

    feat: adsense script

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
+import Script from 'next/script';
 
 import '@/styles/tailwind.css';
 
@@ -22,6 +23,11 @@ export default async function RootLayout({ children }) {
           <main>{children}</main>
           <Footer />
         </div>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3399938065938082"
+          crossOrigin="anonymous"
+        ></Script>
       </body>
     </html>
   );
